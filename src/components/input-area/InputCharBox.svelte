d980e83ebba48c6004d3a3affaf537699190e6a3
kkarlo <kkaragic@gmail.com>
2022-03-06 08:51:37 +0100

    Fix up colors

--- src/components/input-area/InputCharBox.svelte
+++ src/components/input-area/InputCharBox.svelte
@@ -20,17 +20,18 @@
     box-sizing: border-box;
     align-items: center;
     justify-content: center;
-    border: 1px solid var(--on-dark-secondary);
     width: 6vh;
     height: 6vh;
     font-size: 1.7rem;
     font-weight: 600;
     padding: 1px;
-    color: var(--on-light-secondary);
     text-transform: uppercase;
+    border: 1px solid var(--on-light-secondary);
+    color: var(--on-light-secondary);
   }
   :global(body.dark-mode) .wrapper {
     color: var(--on-dark-secondary);
+    border: 1px solid var(--on-dark-secondary);
   }
 
 
