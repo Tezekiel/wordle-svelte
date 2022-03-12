520798e5f2995b6a3b26a0a586f095e0d3cc1756
kkarlo <kkaragic@gmail.com>
2022-03-12 21:18:35 +0100

    Fix bug

--- src/store/storage/storedKeyStates.ts
+++ src/store/storage/storedKeyStates.ts
@@ -6,10 +6,12 @@ export const storedKeyStates: Map<string, CharState> = JSON.parse(localStorage.g
 
 export const saveOrResetKeys = (keyStates: Map<string, CharState>) => {
   if (!gameStoreDate) {
+    localStorage.setItem('storeDate', Date.now().toString())
     localStorage.setItem('keyState', JSON.stringify(new Map<string, CharState>()))
   }
   if (shouldRefresh(Number(gameStoreDate))) {
     localStorage.setItem('keyState', JSON.stringify(new Map<string, CharState>()))
+    localStorage.setItem('storeDate', Date.now().toString())
   } else {
     localStorage.setItem('keyState', JSON.stringify(keyStates))
   }
