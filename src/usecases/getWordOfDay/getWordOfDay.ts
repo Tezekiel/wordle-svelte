a74160e9f712d2cc0128f68c61c8a4e29eef26fe
kkarlo <kkaragic@gmail.com>
2022-03-21 18:42:41 +0100

    Improve seo Add some words

--- src/usecases/getWordOfDay/getWordOfDay.ts
+++ src/usecases/getWordOfDay/getWordOfDay.ts
@@ -1,6 +1,10 @@
 import { diffInDays } from "../getDiffInDays/getDiffInDays";
 
-export const potentialWords = ['dabar', 'klada', 'krava', 'ptica', 'psina', 'vatra', 'glava']
+export const potentialWords = [
+  'dabar', 'klada', 'krava', 'ptica', 'psina', 'vatra', 'glava', 'suton', 'odbor', 'torta',
+  'drven', 'oklop', 'moral', 'šlapa', 'ništa', 'šegrt', 'šverc', 'čavao', 'četka', 'mijeh',
+  'panda', 'spora', 'mikro', 'mikro', 'šugav', 'kičma', 'barij', 'vepar', 'djeca', 'skuta',
+]
 
 const getWordOfDay = (index: number = diffInDays()) => {
   if (index <= 0) index = 0
