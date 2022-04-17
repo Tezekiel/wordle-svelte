d90c1763d712c317c7691cb97b6ea72a149e0cbb
kkarlo <kkaragic@gmail.com>
2022-04-17 09:11:16 +0200

    More words

--- src/usecases/getWordOfDay/getWordOfDay.ts
+++ src/usecases/getWordOfDay/getWordOfDay.ts
@@ -4,7 +4,9 @@ export const potentialWords = [
   'dabar', 'klada', 'krava', 'ptica', 'psina', 'vatra', 'glava', 'suton', 'odbor', 'torta',
   'drven', 'oklop', 'moral', 'šlapa', 'ništa', 'šegrt', 'šverc', 'čavao', 'četka', 'mijeh',
   'panda', 'spora', 'mikro', 'mikro', 'šugav', 'kičma', 'barij', 'vepar', 'djeca', 'skuta',
-  'slava','balet', 'pokop', 'vitak', 'plavi','masno','maska','hotel'
+  'slava', 'balet', 'pokop', 'vitak', 'plavi', 'masno', 'maska', 'hotel', 'hrušt', 'duhan',
+  'dečko', 'anali', 'barok', 'jedem', 'jedro', 'nokti', 'prišt', 'uhiti', 'uhoda', 'viola',
+  'opseg', 'kopno', 'dušik', 'bazga', 'jakna', 'guska', 'harem', 'hidra', 'lažna', 'kutak'
 ]
 
 const getWordOfDay = (index: number = diffInDays()) => {
